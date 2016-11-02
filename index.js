4fcb4f997d16159c67df1d803909d3a37fcf4979
Logan Crandall <crandall.logan@gmail.com>
2016-11-01 23:13:38 -0400

    Began Serverside Dev

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var express = require('express');
 var jade = require('jade');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var request = require('request');
+var parseXML = require('xml2js').parseString;
 
 var app = express(); // Initializes express
 
@@ -21,7 +23,36 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+// Post request from form
+app.post('/', function (req, res) {
+    getRSS(req.body.subreddit, res);
+});
+
+// Get subreddit from url
+app.get('/:subreddit', function (req, res) {
+    getRSS(req.params.subreddit, res);
+});
+
 // Express listens for requests to port PORT
 app.listen(PORT, function () {
     console.log("Express listening on port " + PORT);
 });
+
+// Gets RSS and converts it to a javascript object
+function getRSS(subreddit, res) {
+    request('https://www.reddit.com/r/' + subreddit + '/.rss', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            parseXML(body, function (error, parsedRSS) {
+                if (parsedRSS.feed.title != 'reddit.com: page not found') {
+                    res.render('subreddit', {
+                        subreddit: parsedRSS.feed.title[0],
+                        subtitle: parsedRSS.feed.subtitle[0],
+                        comments: parsedRSS.feed.entry
+                    });
+                } else {
+                    res.send('404');
+                }
+            });
+        }
+    });
+}
